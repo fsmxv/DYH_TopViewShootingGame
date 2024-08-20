25d601847f50cc51ba83210287b545d55891bfbe
fsmxv <fsmxvtjdgh740508@gmail.com>
2024-08-20 20:44:16 +0900

    -프로젝트 기본 세팅 -필레이어 움직임 구현 -플레이어 Follow 카메라(W/시네머신)

---
+++ Assets/02_Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        }
+
+
+    }
+
+}
